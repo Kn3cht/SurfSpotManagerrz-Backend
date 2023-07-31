440a4902fe199d390ebd54597d75e6087e2dedaf
gerald <g.mahlknecht@gmail.com>
2023-07-31 17:53:57 +0200

    rename src to api

---
+++ api/dev.ts
@@ -0,0 +1,6 @@
+/*
+import app from "./index";
+import { defaultSettings } from "./config/defaultSettings";
+
+app.listen(defaultSettings.port, () => console.info("Server started"));
+*/
--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,48 @@
-import httpServer from "../src/index";
+import { ApolloServer } from "apollo-server-express";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import { readFileSync } from "fs";
+import { resolvers } from "./resolvers/gql-resolvers";
+import { connectDB } from "./mongodb";
+import { verifyToken } from "./auth";
+import { SurfSpotManagerrzContext } from "./context";
 
-export default httpServer;
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const schemaPath = path.resolve(__dirname, "./schema.graphql");
+
+const typeDefs = readFileSync(schemaPath, { encoding: "utf-8" });
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const httpServer = createServer(app);
+
+const server = new ApolloServer<SurfSpotManagerrzContext>({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  context: async ({ req }) => {
+    let authorization = req.headers.authorization || null;
+
+    const user = await verifyToken(authorization);
+
+    return {
+      user,
+    };
+  },
+});
+
+const startApolloServer = async (app, httpServer) => {
+  await connectDB();
+
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+await startApolloServer(app, httpServer);
