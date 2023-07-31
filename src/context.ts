84ffa9dcb4daacbfa01d46072ceae2c4e3b2299f
gerald <g.mahlknecht@gmail.com>
2023-07-31 16:29:43 +0200

    wrap apollo server into express

--- src/context.ts
+++ src/context.ts
@@ -1,5 +1,10 @@
 import { User } from "./__generated__/resolvers-types";
 
 export interface SurfSpotManagerrzContext {
+  req: {
+    headers: {
+      authorization: string | undefined;
+    };
+  };
   user: User | null;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServer } from "apollo-server-express";
 import * as path from "path";
 import { fileURLToPath } from "url";
 import { readFileSync } from "fs";
@@ -8,21 +7,27 @@ import { connectDB } from "./mongodb";
 import { verifyToken } from "./auth";
 import { SurfSpotManagerrzContext } from "./context";
 
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const schemaPath = path.resolve(__dirname, "./schema.graphql");
 
 const typeDefs = readFileSync(schemaPath, { encoding: "utf-8" });
 
+const app = express();
+app.use(cors({ origin: "*", credentials: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const httpServer = createServer(app);
+
 const server = new ApolloServer<SurfSpotManagerrzContext>({
   typeDefs,
   resolvers,
-});
-
-await connectDB();
-
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000, path: "/graphql" },
   context: async ({ req }) => {
     let authorization = req.headers.authorization || null;
 
@@ -33,6 +38,20 @@ const { url } = await startStandaloneServer(server, {
     };
   },
 });
-console.log(`🚀  Server ready at: ${url}graphql`);
 
-export default server;
+await connectDB();
+
+await server.start();
+
+server.applyMiddleware({
+  app,
+  cors: false,
+});
+
+httpServer
+  .listen(() =>
+    console.log(`🚀 Server is now running on http://localhost:/graphql`),
+  )
+  .on("close", () => httpServer.close());
+
+export default app;
